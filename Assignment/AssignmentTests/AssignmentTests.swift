95cc1e35f7480901ecf124298224407394ce97e3
Janbaz Ali <janbaz@digitify.com>
2021-02-04 23:45:30 +0500

    Changes for assignment

--- Assignment/AssignmentTests/AssignmentTests.swift
+++ Assignment/AssignmentTests/AssignmentTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import SparkNetworks
+@testable import Assignment
 
-class SparkNetworksTests: XCTestCase {
+class AssignmentTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
